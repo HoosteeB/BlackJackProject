65159d393967a43711bc5bb7ab7e1ed343ebe18f
HoosteeB <94095028+HoosteeB@users.noreply.github.com>
2022-04-14 12:28:00 -0600

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -1,11 +1,27 @@
+
 public class Dealer {
+	
 	Hand hand = new Hand();
 	
-	public Dealer(GameManager game) {
-		this.game = game;
-	}
 	
-	public Hit() {
-		game.DealerDeal();
-	}	
+	public void addDealerCard(Card c) {
+		hand.addCard(c);
+	}
+	public int getDealerTotal(Hand hand) {
+		int total = hand.getTotal();
+		return total;
+	}
+	public int dealersTurn() {
+		int tot = getDealerTotal(hand);
+		while (tot < 17) {
+			addDealerCard(null);
+		} if (tot > 17 && 17 < 22) {
+			return tot;
+		} else if (tot == 17 && hand.contains(Pip.ace)){  
+			x = GameManager.hit;
+			addDealerCard(x);
+		}else {
+			return tot;
+	}
 }
+	
