b4a1a0a4d4ff5c0868071f01025bbddcb95c1fc5
Nick <103149652+nickthenag@users.noreply.github.com>
2022-04-15 20:32:17 -0600

    Update GameMain.java

--- GameMain.java
+++ GameMain.java
@@ -3,7 +3,7 @@ public class GameMain {
 	public static void main(String[] args) {
 		GameManager game = new GameManager();
 		
-		BlackjackGUI GUI = new BlackjackGUI(game);
+		new BlackjackGUI(game);
 		
 	}
 }
