367dc752ba40bc4a334eb438f6e8b19205b882be
Nick <103149652+nickthenag@users.noreply.github.com>
2022-04-19 18:06:17 -0600

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,17 +1,26 @@
 public class Player {
-	Hand hand = new Hand();
-	//GameManager game = new GameManager(null, null);
 	
-	public Player(/*GameManager game*/) {
-		//this.game = game;
+	
+	Hand hand = new Hand();
+
+	GameManager game;
+	
+	public Player(GameManager game) {
+		this.game = game;
 	}
 	
-	public void addPlayerCard() {
-		hand.addCard();
+	public void addPlayerCard(Card c) {
+		hand.addCard(c);
 	}
 	
 	public int getPlayerTotal() {
 		int total = hand.getTotal();
 		return total;
 	}
+	
+	public String toString() {
+		return "player: " + hand;
+	}
+	
+	
 }
