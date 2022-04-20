94fb06fb3a7219a04397411a3f84c92f0a705c0b
Nick <103149652+nickthenag@users.noreply.github.com>
2022-04-20 12:47:53 -0600

    Update Dealer.java

--- Dealer.java
+++ Dealer.java
@@ -1,45 +1,46 @@
 public class Dealer {
-	
-	
+
 	Hand hand = new Hand();
 	GameManager game;
+
 	public Dealer(GameManager game) {
 		this.game = game;
-		
+
 	}
-	
+
 	public void addDealerCard(Card c) {
 		hand.addCard(c);
 	}
+
 	public int getDealerTotal() {
 		int total = hand.getTotal();
 		return total;
 	}
-	public String dealersTurn() {
-		int total = getDealerTotal();
-		while (total < 17) {
+
+	public void dealersTurn() {
+		while (getDealerTotal() < 17) {
 			game.dealerDeal();
-			total = getDealerTotal();
-		} 
-		
-		 if (total >= 17 && total < 22) {
-			return "This card is in beween 17 and 21. Card total is " + total + ".";
-		} else if (total == 17 && hand.contains(FaceValue.ace)){  
-			int newTotal = total- 10;
-			
-			while (newTotal < 10) {
-				game.dealerDeal();
-				newTotal = getDealerTotal();
+		}
+
+		if (getDealerTotal() == 17 && hand.contains(FaceValue.ace)) {
+			game.dealerDeal();
+		} else if (getDealerTotal() > 16 && getDealerTotal() < 22) {
+			System.out.println("Dealers total is in between 17 and 21. Card total is " + getDealerTotal() + ".");
+		} else if (getDealerTotal() > 21 && hand.contains(FaceValue.ace)) {
+			for (Card c : game.dealer.hand.hand) {
+				if (c.getValue() == 11) {
+					c.changeAceValue(c);
+				}
 			}
-			return "This cotains an Ace. Card total is" + total + ".";
-		} else {
-			return "Bust!!! Card total is  " + total + ".";
-		} 
-	
+			while (getDealerTotal() < 17) {
+				game.dealerDeal();
+			}
+		}
+
 	}
-	
+
 	public String toString() {
 		return "dealer: " + hand;
 	}
-	
+
 }
