dc21177dc8df2201d34334ac244df4686b5b2439
Nick <103149652+nickthenag@users.noreply.github.com>
2022-04-25 11:10:58 -0600

    Update GameMain.java

--- GameMain.java
+++ GameMain.java
@@ -1,6 +1,6 @@
 public class GameMain {
 	public static void main(String[] args) {
-		int initialMoney = 1000;
-		GameManager game = new GameManager(initialMoney);		
+		int initalAmount = 1000;
+		GameManager game = new GameManager(initialAmount);		
 	}
 }
